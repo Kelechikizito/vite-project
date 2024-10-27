10aff49fa4be2b7abe5a2b9df7432daf4848ede6
Kelechikizito <kaylaychi77@gmail.com>
2024-10-27 08:16:02 +0100

    files update

--- src/component/JobList.jsx
+++ src/component/JobList.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 const JobList = () => {
   const [data, setData] = useState(null);
-  const ref = useRef(null);
+  const [clickedTags, setClickedTags] = useState([]);
 
   useEffect(() => {
     fetch("/data.json")
@@ -22,75 +22,100 @@ const JobList = () => {
   }, []);
 
   if (!data || !Array.isArray(data)) {
-    return <p>Loading...</p>; // display a loading message while data is being fetched
+    return <p>Loading...</p>;
   }
 
   const tagClickHandler = (event) => {
     const clickedTag = event.target.textContent;
-    console.log(clickedTag)
-  }
+    setClickedTags((prevTags) => [...new Set([...prevTags, clickedTag])]);
+  };
+
+  const Skills = ({ tag }) => (
+    <div className="tag rounded-md p-1 px-2 m-2">
+      <button>{tag}</button>
+    </div>
+  );
 
   return (
-    <ul className="py-16 px-4 sm:px-[10%] bg-[#effafa] min-h-screen flex flex-col gap-10">
-      {data.map((job) => (
-        <li
-          key={job.id}
-          style={job.id === 2 || job.id === 1 ? { borderLeft: '0.5rem solid #5ba4a4' } : {}}
-          className="bg-[#fff] flex flex-col sm:flex-row justify-between py-4 px-4 sm:py-8 sm:px-12 gap-4 sm:gap-0 rounded-lg shadow-2xl"
-        >
-          <div className="flex flex-col sm:flex-row relative gap-6">
-            <div className="absolute sm:relative bottom-[100%] sm:bottom-0 size-[44px] sm:size-[88px]">
-              <img src={job.logo} alt="company-logo" />
-            </div>
+    <div className="relative">
+      {/* Render Skills tags */}
+      <div className="skills-tags flex justify-between absolute top-[-2rem] sm:left-[10%] sm:right-[10%] left-4 right-4  bg-white rounded-lg shadow-2xl py-2 px-4 sm:px-12 ">
+        <div className="flex flex-wrap">
+          {clickedTags.map((tag, index) => (
+            <Skills key={index} tag={tag} />
+          ))}
+        </div>
 
-            <div className="flex flex-col justify-between gap-2 sm:gap-0">
-              <div className="flex gap-4">
-                <p className="font-bold text-[#5ba4a4]">{job.company}</p>
-                <div className="flex gap-2 text-xs">
-                  {job.new ? (
-                    <span className="flex justify-center items-center bg-[#5ba4a4] text-white px-2 rounded-xl">
-                      NEW!
-                    </span>
-                  ) : null}
-                  {job.featured ? (
-                    <span className="flex justify-center items-center px-2 bg-black text-white rounded-xl">
-                      FEATURED
-                    </span>
-                  ) : null}
+        <div className="flex justify-center items-center">
+          <p className="underline underline-offset-1 text-[#5ba4a4] hover:cursor-pointer">Clear</p>
+        </div>
+      </div>
+
+      <ul className="py-16 px-4 sm:px-[10%] bg-[#effafa] min-h-screen flex flex-col gap-10 pt-40 sm:pt-16">
+        {data.map((job) => (
+          <li
+            key={job.id}
+            style={
+              job.id === 2 || job.id === 1
+                ? { borderLeft: "0.5rem solid #5ba4a4" }
+                : {}
+            }
+            className="bg-[#fff] flex flex-col sm:flex-row justify-between py-4 px-4 sm:py-8 sm:px-12 gap-4 sm:gap-0 rounded-lg shadow-2xl"
+          >
+            {/* Job details */}
+            <div className="flex flex-col sm:flex-row relative gap-6">
+              <div className="absolute sm:relative bottom-[100%] sm:bottom-0 size-[44px] sm:size-[88px]">
+                <img src={job.logo} alt="company-logo" />
+              </div>
+              <div className="flex flex-col justify-between gap-2 sm:gap-0">
+                <div className="flex gap-4">
+                  <p className="font-bold text-[#5ba4a4]">{job.company}</p>
+                  <div className="flex gap-2 text-xs">
+                    {job.new && (
+                      <span className="flex justify-center items-center bg-[#5ba4a4] text-white px-2 rounded-xl">
+                        NEW!
+                      </span>
+                    )}
+                    {job.featured && (
+                      <span className="flex justify-center items-center px-2 bg-black text-white rounded-xl">
+                        FEATURED
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <h2 className="text-lg font-bold hover:text-[#5ba4a4] hover:cursor-pointer">
+                  {job.position}
+                </h2>
+                <div className="flex gap-2 text-[#808080]">
+                  <p>{job.postedAt}</p>
+                  <p>·</p>
+                  <p>{job.contract}</p>
+                  <p>·</p>
+                  <p>{job.location}</p>
                 </div>
               </div>
-
-              <h2 className="text-lg font-bold hover:text-[#5ba4a4] hover:cursor-pointer">{job.position}</h2>
-
-              <div className="flex gap-2 text-[#808080]">
-                <p>{job.postedAt}</p>
-                <p>·</p>
-                <p>{job.contract}</p>
-                <p>·</p>
-                <p>{job.location}</p>
-              </div>
             </div>
-          </div>
 
-          <hr className="inline-block sm:hidden border-[1px]" />
+            <hr className="inline-block sm:hidden border-[1px]" />
 
-          <div className="flex flex-wrap items-center justify-start sm:justify-end gap-4 text-[#5ba4a4]">
-            <p className="text-sm font-bold px-2 py-0.5 rounded hover:cursor-pointer hover:text-white hover:bg-[#5ba4a4] bg-[#effafa]" onClick={tagClickHandler} ref={ref}>{job.role}</p>
-            <p className="text-sm font-bold px-2 py-0.5 rounded hover:cursor-pointer hover:text-white hover:bg-[#5ba4a4] bg-[#effafa]" onClick={tagClickHandler} ref={ref}>{job.level}</p>
-            {job.languages.map((language) => (
-              <p className="text-sm font-bold px-2 py-0.5 rounded hover:cursor-pointer hover:text-white hover:bg-[#5ba4a4] bg-[#effafa]" key={language.index} onClick={tagClickHandler} ref={ref}>
-                {language}
-              </p>
-            ))}
-            {job.tools.map((tool) => (
-              <p className="text-sm font-bold px-2 py-0.5 rounded hover:cursor-pointer hover:text-white hover:bg-[#5ba4a4] bg-[#effafa]" key={tool.index} onClick={tagClickHandler} ref={ref}>
-                {tool}
-              </p>
-            ))}
-          </div>
-        </li>
-      ))}
-    </ul>
+            {/* Tags */}
+            <div className="flex flex-wrap items-center justify-start sm:justify-end gap-4 text-[#5ba4a4]">
+              {[job.role, job.level, ...job.languages, ...job.tools].map(
+                (tag, index) => (
+                  <p
+                    key={index}
+                    className="text-sm font-bold px-2 py-0.5 rounded hover:cursor-pointer hover:text-white hover:bg-[#5ba4a4] bg-[#effafa]"
+                    onClick={tagClickHandler}
+                  >
+                    {tag}
+                  </p>
+                )
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
