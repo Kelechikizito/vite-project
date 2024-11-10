48a066647a42f631a08f3d60f70fe6fe28c9165a
Kelechikizito <kaylaychi77@gmail.com>
2024-11-10 17:28:55 +0100

    read me updATE

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,7 @@
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <header className="bg-no-repeat h-36 bg-[#5ba4a4]">
-      {/* Header content */}
-    </header>
-  );
+  return <header className="bg-no-repeat h-36 bg-[#5ba4a4]"></header>;
 };
 
 export default Header;
